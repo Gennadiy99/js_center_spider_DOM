2344549c6b11601c2b04003aa93884709642abbf
Gennadiy99 <login841@gmail.com>
2025-09-01 20:42:26 +0200

    finished project

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-// write code here
+const spider = document.querySelector('.spider');
+
+const spiderStyle = getComputedStyle(spider);
+const spiderWidth = parseInt(spiderStyle.width);
+const spiderHeight = parseInt(spiderStyle.height);
+const field = document.querySelector('.wall');
+const fieldStyle = getComputedStyle(field);
+const fieldWidth = parseInt(fieldStyle.width);
+const fieldHeight = parseInt(fieldStyle.height);
+
+Object.assign(spider.style, {
+  top: `${(fieldHeight - spiderHeight) / 2}px`,
+  left: `${(fieldWidth - spiderWidth) / 2}px`,
+});
