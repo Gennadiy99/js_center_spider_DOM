99a98d60ac338000e6dd836e27affdd8af4bca80
Gennadiy99 <login841@gmail.com>
2025-09-01 21:33:24 +0200

    add if(field)/if(spider)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,21 @@
 
 const spider = document.querySelector('.spider');
 
-const spiderStyle = getComputedStyle(spider);
-const spiderWidth = parseInt(spiderStyle.width);
-const spiderHeight = parseInt(spiderStyle.height);
-const field = document.querySelector('.wall');
-const fieldStyle = getComputedStyle(field);
-const fieldWidth = parseInt(fieldStyle.width);
-const fieldHeight = parseInt(fieldStyle.height);
+if (spider) {
+  const spiderStyle = getComputedStyle(spider);
+  const spiderWidth = parseInt(spiderStyle.width);
+  const spiderHeight = parseInt(spiderStyle.height);
 
-Object.assign(spider.style, {
-  top: `${(fieldHeight - spiderHeight) / 2}px`,
-  left: `${(fieldWidth - spiderWidth) / 2}px`,
-});
+  const field = document.querySelector('.wall');
+
+  if (field) {
+    const fieldStyle = getComputedStyle(field);
+    const fieldWidth = parseInt(fieldStyle.width);
+    const fieldHeight = parseInt(fieldStyle.height);
+
+    Object.assign(spider.style, {
+      top: `${(fieldHeight - spiderHeight) / 2}px`,
+      left: `${(fieldWidth - spiderWidth) / 2}px`,
+    });
+  }
+}
